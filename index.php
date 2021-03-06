7c610b94788031d212f92514f30e130796e97c2d
Shaneth <shanethp@gmail.com>
2021-03-05 22:44:31 -0500

    Added more funny loading texts

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if ($_GET['view'] == "s") {
 <html>
 
 <head>
-    <script></script>
+
     <!---Required tags----->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -45,6 +45,31 @@ if ($_GET['view'] == "s") {
         ?>
     </div>
 </div>
+<script>
+var bodyLoadStatus = false;
+var loadingtexts = ['Launching Pro Tools...', 'Tuning guitars...', 'Waking up the bass player...',
+    'Getting the singer "a glass of slightly below room temperture water"...', 'Turning down the guitar amp...',
+    'Wondering where the guitarist went...', 'Grabbing another slice of pizza...',
+    'Trying to find the buzzing cable...', 'Using a SM57 as a hammer...',
+    'Converting 96KHz WAV to 96kbps MP3...', 'Forgetting about phase...',
+    'Complaining about the line arrays...', 'Waiting for the keyboard player to "find the right sound"...',
+    'Changing the wrong EQ...',
+    'Giving the "drummer more of everything except the lead guitar" in their mix...',
+    'Turning it off and back on again...', 'What key are we in?', 'Tuning the piano...',
+    'Trying to get the B3 to spin up...', 'Sending everything to the reverb...',
+    'Replacing the C800G with a SM7B...', 'Spilling coke on the 1073...',
+    'Using the console to warm up pop tarts...'
+]
+
+function loadingText() {
+    if (!bodyLoadStatus) {
+        var newloadtext = loadingtexts[Math.floor(Math.random() * loadingtexts.length)];
+        document.getElementById('loading-subtext').innerHTML = newloadtext;
+        setTimeout(loadingText, 2000);
+    }
+}
+loadingText();
+</script>
 
 <body onload="bodyLoaded()" data-spy="scroll" data-target="#main-nav">
 
--- js/sp.js
+++ js/sp.js
@@ -2,16 +2,7 @@ document.getElementById("body-content").style.opacity = 0;
 //$("#header").addClass("maxheight"); don't use this now
 $(".nav-sp-link").addClass("background-black");
 //loading textx
-var bodyLoadStatus = false;
-var loadingtexts = ['Launching Pro Tools...', 'Tuning guitars...', 'Waking up the bass player...', 'Getting the singer "a glass of slightly below room temperture water"...', 'Turning down the guitar amp...', 'Wondering where the guitarist went...', 'Grabbing another slice of pizza...', 'Trying to find the buzzing cable...', 'Using a SM57 as a hammer...', 'Converting 96KHz WAV to 96kbps MP3...', 'Forgetting about phase...', 'Complaining about the line arrays...', 'Waiting for the keyboard player to "find the right sound"...', 'Changing the wrong EQ...', 'Giving the "drummer more of everything except the lead guitar" in their mix...', 'Turning it off and back on again...', 'What key are we in?','Tuning the piano...','Trying to get the B3 to spin up...','Sending everything to the reverb...','Replacing the C800G with a SM7B...','Spilling coke on the 1073...','Using the console to warm up pop tarts...']
-function loadingText(){
-    if (!bodyLoadStatus) {
-    var newloadtext=loadingtexts[Math.floor(Math.random() * loadingtexts.length)];
-        document.getElementById('loading-subtext').innerHTML = newloadtext;
-        setTimeout(loadingText, 2000);
-    }
-}
-loadingText();
+
 //set up page
 function setNumMusic(inumMusic){
     numMusic=inumMusic
